bb53cb564944cc3b06f7e3d983cbba8990f003c3
Amirxonoff <jumasevino@gmail.com>
2023-12-13 20:42:36 +0500

    first commit

---
+++ Counter/script.js
@@ -0,0 +1,11 @@
+let p = document.getElementById('p');
+let counter = 0;
+function puls () {
+    p.innerHTML = counter++;
+} 
+function minus () {
+    p.innerHTML = counter--;
+} 
+function remove () {
+    p.innerHTML = removeEventListener = 0;
+} 
